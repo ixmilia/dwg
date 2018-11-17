e8d59019777710d4e97f129539bcf7d95b525d55
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-11-16 16:36:30 -0800

    read/write arcs

---
+++ src/IxMilia.Dwg/Objects/DwgArc.cs
@@ -0,0 +1,21 @@
+﻿namespace IxMilia.Dwg.Objects
+{
+    public partial class DwgArc
+    {
+        /// <summary>
+        /// Create a new arc.
+        /// </summary>
+        /// <param name="center">The center of the arc.</param>
+        /// <param name="radius">The radius of the arc.</param>
+        /// <param name="startAngle">Arc start angle in degrees.</param>
+        /// <param name="endAngle">Arc end angle in degrees.</param>
+        public DwgArc(DwgPoint center, double radius, double startAngle, double endAngle)
+            : this()
+        {
+            Center = center;
+            Radius = radius;
+            StartAngle = startAngle;
+            EndAngle = endAngle;
+        }
+    }
+}
