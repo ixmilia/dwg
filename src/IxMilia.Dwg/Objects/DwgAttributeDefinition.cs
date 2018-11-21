145dc88fec9cc253f8fe68a2123d2a47ac2b6553
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-11-21 11:11:44 -0800

    read/write attribute definitions

---
+++ src/IxMilia.Dwg/Objects/DwgAttributeDefinition.cs
@@ -0,0 +1,13 @@
+﻿namespace IxMilia.Dwg.Objects
+{
+    public partial class DwgAttributeDefinition
+    {
+        public DwgAttributeDefinition(string defaultValue, string tag, string prompt)
+            : this()
+        {
+            DefaultValue = defaultValue;
+            Tag = tag;
+            Prompt = prompt;
+        }
+    }
+}
