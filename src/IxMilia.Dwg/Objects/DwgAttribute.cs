fedf9b5507e8933a5db81bf9bfd4f32f2a8ed8da
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-11-21 11:07:16 -0800

    read/write attributes

---
+++ src/IxMilia.Dwg/Objects/DwgAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace IxMilia.Dwg.Objects
+{
+    public partial class DwgAttribute
+    {
+        public DwgAttribute(string value)
+            : this()
+        {
+            Value = value;
+        }
+    }
+}
