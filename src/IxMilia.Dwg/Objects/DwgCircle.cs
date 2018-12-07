dcbb1a6841835328e6d3b84d2cac3c0be74ff69a
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-12-06 16:00:52 -0800

    read/write circles

---
+++ src/IxMilia.Dwg/Objects/DwgCircle.cs
@@ -0,0 +1,17 @@
+﻿namespace IxMilia.Dwg.Objects
+{
+    public partial class DwgCircle
+    {
+        /// <summary>
+        /// Create a new circle.
+        /// </summary>
+        /// <param name="center">The center of the circle.</param>
+        /// <param name="radius">The radius of the circle.</param>
+        public DwgCircle(DwgPoint center, double radius)
+            : this()
+        {
+            Center = center;
+            Radius = radius;
+        }
+    }
+}
