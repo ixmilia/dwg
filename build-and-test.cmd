06f0e9d6680dff8358e50e46c15aea122b09e05a
Brett V. Forsgren <brett.forsgren@outlook.com>
2019-10-15 11:02:12 -0700

    allow building Debug/Release

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -1,4 +1,28 @@
 @echo off
+setlocal
+
+set thisdir=%~dp0
+set configuration=Debug
+set runtests=true
+
+:parseargs
+if "%1" == "" goto argsdone
+if /i "%1" == "-c" (
+    set configuration=%2
+    shift
+    shift
+    goto parseargs
+)
+if /i "%1" == "-notest" (
+    set runtests=false
+    shift
+    goto parseargs
+)
+
+echo Unsupported argument: %1
+goto error
+
+:argsdone
 
 :: run code generator
 set GENERATOR_DIR=%~dp0src\IxMilia.Dwg.generator
@@ -12,14 +36,15 @@ dotnet run "%LIBRARY_DIR%"
 if errorlevel 1 goto error
 popd
 
-:: build and run tests
-set TEST_PROJECT=.\src\IxMilia.Dwg.Test\IxMilia.Dwg.Test.csproj
-dotnet restore %TEST_PROJECT%
+:: build
+set SOLUTION=%~dp0src\IxMilia.Dwg.sln
+dotnet restore %SOLUTION%
 if errorlevel 1 exit /b 1
-dotnet test %TEST_PROJECT%
+dotnet build %SOLUTION% -c %configuration%
 if errorlevel 1 exit /b 1
-goto :eof
 
-error:
-echo Error building project.
-exit /b 1
+:: test
+if /i "%runtests%" == "true" (
+    dotnet test "%SOLUTION%" -c %configuration% --no-restore --no-build
+    if errorlevel 1 goto error
+)
--- build-and-test.sh
+++ build-and-test.sh
@@ -2,6 +2,26 @@
 
 _SCRIPT_DIR="$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"
 
+CONFIGURATION=Debug
+RUNTESTS=true
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --configuration|-c)
+      CONFIGURATION=$2
+      shift
+      ;;
+    --notest)
+      RUNTESTS=false
+      ;;
+    *)
+      echo "Invalid argument: $1"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
 # run code generator
 GENERATOR_DIR=$_SCRIPT_DIR/src/IxMilia.Dwg.Generator
 LIBRARY_DIR=$_SCRIPT_DIR/src/IxMilia.Dwg
@@ -11,8 +31,12 @@ dotnet build
 dotnet run "$LIBRARY_DIR"
 cd -
 
-# build and run tests
-TEST_PROJECT=$_SCRIPT_DIR/src/IxMilia.Dwg.Test/IxMilia.Dwg.Test.csproj
-dotnet restore "$TEST_PROJECT"
-dotnet build "$TEST_PROJECT"
-dotnet test "$TEST_PROJECT"
+# build
+SOLUTION=$_SCRIPT_DIR/src/IxMilia.Converters.sln
+dotnet restore $SOLUTION
+dotnet build $SOLUTION -c $CONFIGURATION
+
+# test
+if [ "$RUNTESTS" = "true" ]; then
+    dotnet test $SOLUTION -c $CONFIGURATION --no-restore --no-build
+fi
