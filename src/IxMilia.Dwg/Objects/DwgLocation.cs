4b2a8c1830b96d9ee5df645fc0b6521ffa6f02af
Brett V. Forsgren <brett.forsgren@outlook.com>
2020-09-09 13:22:30 -0700

    read/write location

---
+++ src/IxMilia.Dwg/Objects/DwgLocation.cs
@@ -0,0 +1,11 @@
+﻿namespace IxMilia.Dwg.Objects
+{
+    public partial class DwgLocation
+    {
+        public DwgLocation(DwgPoint point)
+            : this()
+        {
+            Point = point;
+        }
+    }
+}
