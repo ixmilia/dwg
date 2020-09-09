95c9f2700c8e9ad9775ccd24c60be59fdb0a616e
Brett V. Forsgren <brett.forsgren@outlook.com>
2020-09-09 13:16:26 -0700

    read/write ray

---
+++ src/IxMilia.Dwg/Objects/DwgRay.cs
@@ -0,0 +1,12 @@
+﻿namespace IxMilia.Dwg.Objects
+{
+    public partial class DwgRay
+    {
+        public DwgRay(DwgPoint point, DwgVector vector)
+            : this()
+        {
+            Point = point;
+            Vector = vector;
+        }
+    }
+}
