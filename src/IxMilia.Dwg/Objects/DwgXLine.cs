44884150196e9e5e19d091e4629424dd703e8c9d
Brett V. Forsgren <brett.forsgren@outlook.com>
2020-09-09 13:17:58 -0700

    read/write xline

---
+++ src/IxMilia.Dwg/Objects/DwgXLine.cs
@@ -0,0 +1,12 @@
+﻿namespace IxMilia.Dwg.Objects
+{
+    public partial class DwgXLine
+    {
+        public DwgXLine(DwgPoint point, DwgVector vector)
+            : this()
+        {
+            Point = point;
+            Vector = vector;
+        }
+    }
+}
