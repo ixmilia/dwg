24aecf30588c8880d42baae0d2ed260a7eb7c571
Brett V. Forsgren <brett.forsgren@outlook.com>
2020-09-23 12:37:17 -0700

    add more object read tests

---
+++ src/IxMilia.Dwg/Objects/DwgFace3D.cs
@@ -0,0 +1,14 @@
+﻿namespace IxMilia.Dwg.Objects
+{
+    public partial class DwgFace3D
+    {
+        public DwgFace3D(DwgPoint firstCorner, DwgPoint secondCorner, DwgPoint thirdCorder, DwgPoint fourthCorner)
+            : this()
+        {
+            FirstCorner = firstCorner;
+            SecondCorner = secondCorner;
+            ThirdCorner = thirdCorder;
+            FourthCorner = fourthCorner;
+        }
+    }
+}
