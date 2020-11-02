39975b2e717150f14c095e91acb10b98e13af38d
Brett V. Forsgren <brett.forsgren@outlook.com>
2020-11-02 12:59:40 -0800

    make build script more generic

--- build-and-test.ps1
+++ build-and-test.ps1
@@ -13,16 +13,28 @@ function Fail([string]$message) {
     throw $message
 }
 
+function Single([string]$pattern) {
+    $items = @(Get-Item $pattern)
+    if ($items.Length -ne 1) {
+        $itemsList = $items -Join "`n"
+        Fail "Expected single item, found`n$itemsList`n"
+    }
+
+    return $items[0]
+}
+
 try {
     # run code generator
-    Push-Location "$PSScriptRoot/src/IxMilia.Dwg.Generator"
+    $generatorLocation = Single "$PSScriptRoot/src/*.Generator"
+    $generatorDestination = $generatorLocation.FullName -Replace ".Generator$", ""
+    Push-Location $generatorLocation
     dotnet restore || (Pop-Location && Fail "Failed to restore generator.")
     dotnet build || (Pop-Location && Fail "Failed to build generator.")
-    dotnet run -- "$PSScriptRoot/src/IxMilia.Dwg" || (Pop-Location && Fail "Failed to run generator.")
+    dotnet run -- $generatorDestination || (Pop-Location && Fail "Failed to run generator.")
     Pop-Location
 
     # build
-    $solution = (Get-Item "$PSScriptRoot/IxMilia.*.sln")[0]
+    $solution = Single "$PSScriptRoot/*.sln"
     dotnet restore $solution || Fail "Failed to restore solution"
     dotnet build $solution --configuration $configuration || Fail "Failed to build solution"
 
@@ -33,6 +45,8 @@ try {
 
     # create package
     dotnet pack --no-restore --no-build --configuration $configuration $solution || Fail "Error creating package."
+    $package = Single "$PSScriptRoot/artifacts/packages/$configuration/*.nupkg"
+    Write-Host "Package generated at '$package'"
 }
 catch {
     Write-Host $_
