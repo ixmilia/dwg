bf2e46d017272d4506211f9878b9e2ab27245957
Brett V. Forsgren <brett.forsgren@outlook.com>
2021-07-01 11:30:56 -0600

    read/write view port entity

---
+++ src/IxMilia.Dwg/Objects/DwgViewPortEntity.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace IxMilia.Dwg.Objects
+{
+    public partial class DwgViewPortEntity
+    {
+        public DwgViewPortEntityHeader ViewPortEntityHeader { get; set; }
+
+        private DwgHandleReference _viewPortEntityHeaderHandleReference;
+
+        internal override IEnumerable<DwgObject> ChildItems
+        {
+            get
+            {
+                yield return ViewPortEntityHeader;
+            }
+        }
+
+        internal override void ReadPostData(BitReader reader)
+        {
+            _viewPortEntityHeaderHandleReference = reader.Read_H();
+            if (_viewPortEntityHeaderHandleReference.Code != DwgHandleReferenceCode.SoftOwner)
+            {
+                throw new DwgReadException("Incorrect view port entity header handle code.");
+            }
+        }
+
+        internal override void OnAfterEntityRead(BitReader reader, DwgObjectCache objectCache)
+        {
+            ViewPortEntityHeader = objectCache.GetObject<DwgViewPortEntityHeader>(reader, ResolveHandleReference(_viewPortEntityHeaderHandleReference));
+        }
+
+        internal override void OnBeforeEntityWrite()
+        {
+            _viewPortEntityHeaderHandleReference = ViewPortEntityHeader.MakeHandleReference(DwgHandleReferenceCode.SoftOwner);
+        }
+
+        internal override void WritePostData(BitWriter writer)
+        {
+            writer.Write_H(_viewPortEntityHeaderHandleReference);
+        }
+    }
+}
