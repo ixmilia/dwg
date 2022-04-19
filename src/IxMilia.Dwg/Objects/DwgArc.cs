48d72f98132fb661f8f32b50e4d13180487592fd
Brett V. Forsgren <brett.forsgren@outlook.com>
2022-04-19 12:38:29 -0600

    fix comments for arc angles

--- src/IxMilia.Dwg/Objects/DwgArc.cs
+++ src/IxMilia.Dwg/Objects/DwgArc.cs
@@ -7,8 +7,8 @@
         /// </summary>
         /// <param name="center">The center of the arc.</param>
         /// <param name="radius">The radius of the arc.</param>
-        /// <param name="startAngle">Arc start angle in degrees.</param>
-        /// <param name="endAngle">Arc end angle in degrees.</param>
+        /// <param name="startAngle">Arc start angle in radians.</param>
+        /// <param name="endAngle">Arc end angle in radians.</param>
         public DwgArc(DwgPoint center, double radius, double startAngle, double endAngle)
             : this()
         {
